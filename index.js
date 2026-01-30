8534f44f4d56431ff4d91b6b36d44129fc277d56
AllenCodess <Aph296@protonmail.com>
2026-01-30 12:42:47 -0500

    completed the away buttons js

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-// created variables of selected elements
-// event listeners
-// functions
 
-// home
 let homeScore = 0
 let homeScoreEl = document.querySelector(".home-score")
 homeScoreEl.textContent = 0
+
+let awayScore = 0
+let awayScoreEl = document.querySelector(".away-score")
+awayScoreEl.textContent = 0
+
+const awayTouchdownEl = document.querySelector("#away-touchdown")
+const awayExtraEl = document.querySelector("#away-extra")
+const awayFgEl = document.querySelector("#away-fg")
 
 const homeTouchdownEl = document.querySelector("#home-touchdown")
 const homeExtraEl = document.querySelector("#home-extra")
 const homeFgEl = document.querySelector("#home-fg")
 
+// event listeners
+awayTouchdownEl.addEventListener("click", awayTouchdownScore)
+awayExtraEl.addEventListener("click", awayExtra)
+awayFgEl.addEventListener("click", awayFieldGoal)
 
 homeTouchdownEl.addEventListener("click", homeTouchdownScore)
 homeExtraEl.addEventListener("click", homeExtra)
 homeFgEl.addEventListener("click", FieldGoal)
 
 
-
+// functions
 function homeTouchdownScore () {
     homeScore += 6
     homeScoreEl.textContent = homeScore
@@ -33,6 +41,19 @@ function FieldGoal () {
     homeScoreEl.textContent = homeScore
 }
 
+function awayTouchdownScore () {
+    awayScore += 6
+    awayScoreEl.textContent = awayScore
+}
 
+function awayExtra () {
+    awayScore += 1
+    awayScoreEl.textContent = awayScore
+}
+
+function awayFieldGoal () {
+    awayScore += 3
+    awayScoreEl.textContent = awayScore
+}
 
 
