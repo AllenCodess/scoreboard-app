95ab41ce0dca386e9795136b58a1420899efaefb
AllenCodess <Aph296@protonmail.com>
2026-01-30 13:19:38 -0500

    completed the winner message for both teams

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let awayScore = 0
 let awayScoreEl = document.querySelector(".away-score")
 awayScoreEl.textContent = 0
 
+const winnerMessage = document.querySelector("#winner-message")
+
 const awayTouchdownEl = document.querySelector("#away-touchdown")
 const awayExtraEl = document.querySelector("#away-extra")
 const awayFgEl = document.querySelector("#away-fg")
@@ -25,35 +27,50 @@ homeExtraEl.addEventListener("click", homeExtra)
 homeFgEl.addEventListener("click", FieldGoal)
 
 
+function winnerMessages () {
+    if (homeScore >= 42) {
+        winnerMessage.textContent = "Home Team Wins!!!!"
+    } else if (awayScore >= 42) {
+        winnerMessage.textContent = "Away Team Wins!!!"
+    }
+}
+
+
 // functions
 function homeTouchdownScore () {
     homeScore += 6
     homeScoreEl.textContent = homeScore
+    winnerMessages()
 }
 
 function homeExtra () {
     homeScore += 1
     homeScoreEl.textContent = homeScore
+    winnerMessages()
 }
 
 function FieldGoal () {
     homeScore += 3
     homeScoreEl.textContent = homeScore
+    winnerMessages()
 }
 
 function awayTouchdownScore () {
     awayScore += 6
     awayScoreEl.textContent = awayScore
+    winnerMessages()
 }
 
 function awayExtra () {
     awayScore += 1
     awayScoreEl.textContent = awayScore
+    winnerMessages()
 }
 
 function awayFieldGoal () {
     awayScore += 3
     awayScoreEl.textContent = awayScore
+    winnerMessages()
 }
 
 
